0c9b3a0e0a23e57ec3984b1601ba9ca9bba6aead
IMdCODr <imdcodr@gmail.com>
2022-01-23 18:17:55 +0530

    Create gamers.js

---
+++ gamers.js
@@ -0,0 +1,15 @@
+function showcards() {
+
+    let ogc = document.getElementById('ourgamerscard');
+    let og = document.getElementById('home')
+    let btn = document.getElementById('ogbtn')
+
+
+    if (ogc.style.display != 'none') {
+
+        ogc.style.display = 'block';
+        og.style.display = 'none';
+
+    }
+
+}
